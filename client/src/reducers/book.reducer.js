d449a3e71393765bbf1a53b84e7bbd119fab8532
NikolaKostadinov <nikola@kostadinov@protonmail.com>
2022-07-12 22:19:27 +0300

    Empty state before action

--- client/src/reducers/book.reducer.js
+++ client/src/reducers/book.reducer.js
@@ -3,6 +3,7 @@ export default function reducer (state=[], action) {
     switch (action.type) {
     
         case 'GET':
+            state = []
             return state.concat(action.payload);
 
         default: return state;
